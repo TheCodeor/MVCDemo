43f7c85cf61b9ced9704c9664c38d05af660237f
TheCodeor <447690182@qq.com>
2017-03-02 16:07:29 +0800

    Initial Commit

---
+++ MVCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVCDemo
+//
+//  Created by fanpeng on 17/3/2.
+//  Copyright © 2017年 fanpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVCDemo
+//
+//  Created by fanpeng on 17/3/2.
+//  Copyright © 2017年 fanpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
